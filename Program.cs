306e9132104878ba7d31f067243857fb081e0133
Jose Luis Ordaz <jlordsan@hotmail.com>
2021-11-30 09:59:45 -0400

    Update to .NET 6

--- Program.cs
+++ Program.cs
@@ -1,85 +1,74 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
+﻿// See https://aka.ms/new-console-template for more information
+string rutaDestino;
+string ruta = @"%UserProfile%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets";
+string rutaFull = Environment.ExpandEnvironmentVariables(ruta);
 
-namespace SaveWSpotlightImage
+string[] archivos = System.IO.Directory.GetFiles(rutaFull);
+string destino;
+int contarNuevo = 0;
+int contarExistentes = 0;
+if (args.Length > 0)
 {
-    class Program
+    rutaDestino = args[0];
+    Console.WriteLine("Starting...");
+    ConfigureFolders();
+
+    Console.WriteLine("Searching for files");
+
+    foreach (var item in archivos)
     {
-        static string rutaDestino;
-        static string ruta = @"%UserProfile%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets";
-        static string rutaFull = Environment.ExpandEnvironmentVariables(ruta);
-        static void Main(string[] args)
+        Console.Write(".");
+        FileInfo fileInfo = new FileInfo(item);
+        if (fileInfo.Length > 100000)
         {
-            string[] archivos = System.IO.Directory.GetFiles(rutaFull);
-            string destino;
-            int contarNuevo = 0;
-            int contarExistentes = 0;
-            if (args.Length > 0)
+            var image = System.Drawing.Image.FromFile(fileInfo.FullName);
+
+            if (image.Size.Width > image.Size.Height)
             {
-                rutaDestino = args[0];
-                Console.WriteLine("Starting...");
-                ConfigureFolders();
-
-                Console.WriteLine("Searching for files");
-
-                foreach (var item in archivos)
-                {
-                    Console.Write(".");
-                    FileInfo fileInfo = new FileInfo(item);
-                    if (fileInfo.Length > 100000)
-                    {
-                        var image = System.Drawing.Image.FromFile(fileInfo.FullName);
-
-                        if (image.Size.Width > image.Size.Height)
-                        {
-                            destino = Path.Combine(rutaDestino, "landscape", fileInfo.Name + ".jpg");
-                        }
-                        else
-                        {
-                            destino = Path.Combine(rutaDestino, "portrait", fileInfo.Name + ".jpg");
-                        }
-
-
-                        if (File.Exists(destino))
-                            contarExistentes++;
-                        else
-                            contarNuevo++;
-
-                        File.Copy(fileInfo.FullName, destino, true);
-                    }
-                };
-                Console.WriteLine();
-                Console.WriteLine(value: $"Found \n{contarNuevo} new files.. \n{contarExistentes} existing files..");
+                destino = Path.Combine(rutaDestino, "landscape", fileInfo.Name + ".jpg");
             }
             else
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Please indicate as a parameter the destination route");
-                Console.ForegroundColor = ConsoleColor.White;
+                destino = Path.Combine(rutaDestino, "portrait", fileInfo.Name + ".jpg");
             }
+
+
+            if (File.Exists(destino))
+                contarExistentes++;
+            else
+                contarNuevo++;
+
+            File.Copy(fileInfo.FullName, destino, true);
         }
+    };
+    Console.WriteLine();
+    Console.WriteLine(value: $"Found \n{contarNuevo} new files.. \n{contarExistentes} existing files..");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Please indicate as a parameter the destination route");
+    Console.ForegroundColor = ConsoleColor.White;
+}
 
 
-        private static void ConfigureFolders()
+void ConfigureFolders()
+{
+    if (!Directory.Exists(rutaDestino))
+    {
+        Console.WriteLine("Creating destination folder");
+        Directory.CreateDirectory(rutaDestino);
+
+        if (!Directory.Exists(Path.Combine(rutaDestino, "landscape")))
         {
-            if (!Directory.Exists(rutaDestino))
-            {
-                Console.WriteLine("Creating destination folder");
-                Directory.CreateDirectory(rutaDestino);
-
-                if (!Directory.Exists(Path.Combine(rutaDestino, "landscape")))
-                {
-                    Console.WriteLine("Creating folder landscape");
-                    Directory.CreateDirectory(Path.Combine(rutaDestino, "landscape"));
-                }
-                if (!Directory.Exists(Path.Combine(rutaDestino, "portrait")))
-                {
-                    Console.WriteLine("Creating folder landscape");
-                    Directory.CreateDirectory(Path.Combine(rutaDestino, "portrait"));
-                }
-            }
-
+            Console.WriteLine("Creating folder landscape");
+            Directory.CreateDirectory(Path.Combine(rutaDestino, "landscape"));
+        }
+        if (!Directory.Exists(Path.Combine(rutaDestino, "portrait")))
+        {
+            Console.WriteLine("Creating folder landscape");
+            Directory.CreateDirectory(Path.Combine(rutaDestino, "portrait"));
         }
     }
+
 }
--- runapp.cmd
+++ runapp.cmd
@@ -1,1 +1,1 @@
-dotnet run D:\temp\SpotlightImages
+D:\VSProjects\SaveWSpotlightImage\bin\Release\net6.0-windows\win-x64\publish\SaveWSpotlightImage.exe D:\temp\SpotlightImages
